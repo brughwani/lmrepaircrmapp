94e85c67b8cea85b1d9d40fdafe79905e20141a0
brughwani <brughwani@gmail.com>
2024-09-29 19:06:23 +0530

    0.9

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
-=======
+
 import 'package:flutter/material.dart';
 import 'package:lmrepaircrmapp/LoginPage.dart';
 
 void main() {
   runApp(MyHomePage());
 }
-
->>>>>>> eec56b5 (added login page)
